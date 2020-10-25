355c18eb5020f665eaf47b2b08d2c0d933eb9587
ayaney85 <osma8251@stthomas.edu>
2020-10-25 14:54:27 -0500

    refactor and still passing test_PD_Check_for_Skunk

--- SkunkProject/test/skunk/domain/TestPredictableDie.java
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -133,15 +133,10 @@ public class TestPredictableDie
 	@Test
 	public void test_PD_Check_for_Skunk()
 	{
-		PredictableDie die = new PredictableDie (new int[] {1,2});
+		PredictableDie die = new PredictableDie (new int[] {CONSTANT_SKUNK});
 		
 		die.roll();
-		int firstroll = die.getLastRoll();
-		
-		die.roll();
-		int secondroll = die.getLastRoll();
-		
-		assertTrue(firstroll == 1 || secondroll == 1);
+		assertEquals(CONSTANT_SKUNK, die.getLastRoll());
 	}
 
 
