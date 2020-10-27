09b4f814245b96081938d9799872062db2d9c639
ayaney85 <osma8251@stthomas.edu>
2020-10-26 23:42:54 -0500

    Passing test for test_PD_Check_for_Skunk

--- SkunkProject/test/skunk/domain/TestPredictableDie.java
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -132,7 +132,15 @@ public class TestPredictableDie
 	@Test
 	public void test_PD_Check_for_Skunk()
 	{
-		fail();
+		PredictableDie die = new PredictableDie (new int[] {1,2});
+
+		die.roll();
+		int firstroll = die.getLastRoll();
+
+		die.roll();
+		int secondroll = die.getLastRoll();
+
+		assertTrue(firstroll == 1 || secondroll == 1);
 	}
 
 }
