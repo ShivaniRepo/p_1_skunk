a0384e4aed0918611ab21e63507d71bdb97ce2f3
ShivaniProjects <shivani.jawda@seagate.com>
2020-10-31 20:07:39 -0500

    Added failing test_skunk_controller_run() to check if run() gets called newly added class SkunkController.

---
+++ SkunkProject/test/skunk/domain/TestSkunkController.java
@@ -0,0 +1,22 @@
+package skunk.domain;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TestSkunkController
+{
+	@Test
+	public void test_skunk_controller_run()
+	{
+		fail();
+		
+		SkunkController skunk = new SkunkController();
+
+		boolean bStatus = skunk.run();
+
+		assertFalse(bStatus);
+	}
+}
