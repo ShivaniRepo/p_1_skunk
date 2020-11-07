f0ea63f9d85043706e4650ca55e1d44e83d4b400
ayaney85 <osma8251@stthomas.edu>
2020-11-06 23:27:30 -0600

    1st failing test for verifying Player's score

--- SkunkProject/test/skunk/domain/TestPlayer.java
+++ SkunkProject/test/skunk/domain/TestPlayer.java
@@ -6,9 +6,12 @@ import org.junit.Test;
 
 public class TestPlayer
 {
+	Player player = new Player();	
+	
+	// Test to verify Player's score
 
 	@Test
-	public void test()
+	public void testPlayer1()
 	{
 		fail("Not yet implemented");
 	}
