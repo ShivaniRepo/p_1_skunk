18af8ee311ca60a8c61cf284e37c7b3b36f280b4
ayaney85 <osma8251@stthomas.edu>
2020-11-06 23:18:59 -0600

    Created TestPlayer Class

---
+++ SkunkProject/test/skunk/domain/TestPlayer.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPlayer
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
