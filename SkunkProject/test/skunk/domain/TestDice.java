5b6be92beeaa78b709c96c1a2cd01cccee419548
quannytsunami <42845033+quannytsunami@users.noreply.github.com>
2020-11-13 16:30:38 -0600

    Added TestDice class and new JUnit test case (#18)
    * Created TestPlayer Class
    
    * 1st failing test for verifying Player's score
    
    * Passing test for testplayer1 verifying score starts at 0
    
    * Failing test for to verify player's name
    
    * 2nd failing test for testPlayer2 - verifying players Name
    
    * Passing test for testplayer2 to verify player's name
    
    * Failing test for testPlayerString to test if a player's name is a string
    
    * Passing test for to test if player's name is a string
    
    * failing test for testing setting # of chips of player
    
    * Passing test for testing setPlayerChipCount
    
    * refactor the player class so that my test cases could work
    
    * Added states to the Player class default constructor.
    
    * Updated Game and TestGame with revised methods.
    
    * Added new test case test_end_game(). First failing test.
    
    * test_end_game(). Passing test.
    
    * test_end_game(). Refactored. Passing test.
    
    * test_game_total_player(). Failing test.
    
    * test_game_total_player(). Refactored. Passing test.
    
    * Added additional comments in TestGame.
    
    * test_game_add_player(). Refactored. Passing test.
    
    * test_game_remove_player(). Refactored. Failing test.
    
    * test_game_remove_player(). Passing test with 3 players added.
    
    * test_game_remove_player(). Added remove method. Failing test.
    
    * test_game_remove_player(). Refactored. Passing test.
    
    * test_game_object(). First failing test.
    
    * test_game_object(). Refactored. Failing test.
    
    * test_game_object(). Refactored. Passing test.
    
    * test_game_object(). Removed some logic. Passing test.
    
    * Added TestDice class. Createdtest case test_Dice_Roll_Value(). Failing.
    
    * test_Dice_Roll_Value(). Passing test.
    
    * test_Dice_Roll_Value(). Refactored. Passing.
    
    * test_Dice_Roll_Value(). Refactored again. Passing.
    
    Co-authored-by: ayaney85 <osma8251@stthomas.edu>

---
+++ SkunkProject/test/skunk/domain/TestDice.java
@@ -0,0 +1,17 @@
+package skunk.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDice {
+	
+	@Test
+	public void test_Dice_Roll_Value() 
+	{
+		Dice dice = new Dice();
+		dice.roll();
+		assertTrue(dice.getLastRoll() >= 2 && dice.getLastRoll() <= 12);
+	}
+
+}
--- SkunkProject/test/skunk/domain/TestDie.java
+++ SkunkProject/test/skunk/domain/TestDie.java
@@ -9,13 +9,11 @@ import org.junit.Test;
 public class TestDie
 {
 	@Test
-	public void test_Roll_Value()
+	public void test_Die_Roll_Value()
 	{
 		Die die = new Die();
 		die.roll();
         assertTrue(die.getLastRoll() >= 1 && die.getLastRoll() <= 6);
 	}
 	
-
-
 }
