9bab0a7fe6840c4fc6578940ebbaf2a833e0dafe
quannytsunami <nguy1491@gmail.com>
2020-11-13 16:20:55 -0600

    Added TestDice class. Createdtest case test_Dice_Roll_Value(). Failing.

---
+++ SkunkProject/test/skunk/domain/TestDice.java
@@ -0,0 +1,15 @@
+package skunk.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDice {
+	
+	@Test
+	public void test_Dice_Roll_Value() 
+	{
+		fail();
+	}
+
+}
