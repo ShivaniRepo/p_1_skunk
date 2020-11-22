56543e7c72094b8b384e02de5639b70e8d4bac1f
quannytsunami <nguy1491@gmail.com>
2020-11-22 17:32:01 -0600

    test_game_boolean_run(). Passing test. Need to refactor again.

--- SkunkProject/test/skunk/domain/TestGame.java
+++ SkunkProject/test/skunk/domain/TestGame.java
@@ -2,6 +2,7 @@ package skunk.domain;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
 import java.util.Random;
 
 import org.junit.Test;
@@ -11,62 +12,70 @@ public class TestGame {
 	private static final int RETURN_OK = 0;
 	private static final int ERROR_INVALID_PLAYER_NUMBER = -1;
 
+//	@Test
+//	public void test_game_status() 
+//	{
+//		Game newGame = new Game();
+//		String gameStatus;
+//		
+//		// Test to verify game NOT started.
+//		
+//		gameStatus = newGame.getGameStatus();
+//		assertEquals("Skunk game has NOT started.", gameStatus);
+//		
+//		// Test to verify game started.
+//		
+//		newGame.startGame();
+//		gameStatus = newGame.getGameStatus();
+//		assertEquals("Skunk game has started.", gameStatus);
+//		
+//	}
+//
+//	// Test to verify game has ended. 
+//	
+//	@Test
+//	public void test_end_game()
+//	{
+//		Game newGame = new Game();
+//		boolean gameStatus;
+//		
+//		gameStatus = newGame.endGame();
+//		assertTrue(gameStatus == false);
+//	}
+//	
+//	//  Test to ADD Player to Skunk Game. 
+//	
+//	@Test
+//	public void test_game_invalid_player_number_negative()
+//	{
+//		Game newGame = new Game();
+//		
+//		int iReturn = newGame.getNumberOfPlayers(-1);
+//		assertEquals(ERROR_INVALID_PLAYER_NUMBER, iReturn);
+//	}
+//
+//	@Test
+//	public void test_skunk_game_valid_player_number_zero_to_Max()
+//	{
+//		Game newGame = new Game();
+//		Random rand = new Random();
+//		
+//		int min = 1;
+//		int max = 30;
+//		
+//		int iPlayerCount = rand.nextInt(max - min + 1) + min;
+//		
+//		int iReturn = newGame.getNumberOfPlayers( iPlayerCount );
+//		assertEquals( RETURN_OK, iReturn );
+//	}
 	@Test
-	public void test_game_status() 
+	public void test_game_boolean_run()
 	{
 		Game newGame = new Game();
-		String gameStatus;
-		
-		// Test to verify game NOT started.
-		
-		gameStatus = newGame.getGameStatus();
-		assertEquals("Skunk game has NOT started.", gameStatus);
-		
-		// Test to verify game started.
-		
-		newGame.startGame();
-		gameStatus = newGame.getGameStatus();
-		assertEquals("Skunk game has started.", gameStatus);
-		
+		boolean expected = newGame.run();
+		assertEquals(true, expected);
 	}
-
-	// Test to verify game has ended. 
 	
-	@Test
-	public void test_end_game()
-	{
-		Game newGame = new Game();
-		boolean gameStatus;
-		
-		gameStatus = newGame.endGame();
-		assertTrue(gameStatus == false);
-	}
-	
-	//  Test to ADD Player to Skunk Game. 
-	
-	@Test
-	public void test_game_invalid_player_number_negative()
-	{
-		Game newGame = new Game();
-		
-		int iReturn = newGame.getNumberOfPlayers(-1);
-		assertEquals(ERROR_INVALID_PLAYER_NUMBER, iReturn);
-	}
-
-	@Test
-	public void test_skunk_game_valid_player_number_zero_to_Max()
-	{
-		Game newGame = new Game();
-		Random rand = new Random();
-		
-		int min = 1;
-		int max = 30;
-		
-		int iPlayerCount = rand.nextInt(max - min + 1) + min;
-		
-		int iReturn = newGame.getNumberOfPlayers( iPlayerCount );
-		assertEquals( RETURN_OK, iReturn );
-	}
 }
 
 //	//  Test to ADD Player to Skunk Game. 
