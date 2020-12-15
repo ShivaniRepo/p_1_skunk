777b3e2a08916942cdbe2f49cdc405e4cffe85ba
ayaney85 <71532115+ayaney85@users.noreply.github.com>
2020-12-15 16:46:59 -0600

    Updated the class diagram to reflect the changes made for P1.3 (#35)
    * Created a class Diagram for SkunkProject
    
    * Update Class Diagram to remove Scratch class
    
    * removed the spacing at the end of testgame class
    
    * removed the uncommenting of TestSkunkApp
    
    * Cleaned up the class diagram to match up with all the changes for P1.3

--- SkunkProject/test/skunk/domain/TestSkunkApp.java
+++ SkunkProject/test/skunk/domain/TestSkunkApp.java
@@ -8,15 +8,13 @@ import org.junit.Test;
 
 public class TestSkunkApp 
 {
-//	private static final int CONSTANT_RUN_SUCCESSFUL = 100;
-//	
-//	
-//	@Test
-//	public void testing_SkunkApp_main()
-//	{
-//		assertEquals( SkunkApp.getiTestValue_ForApp(), -1 );
-//		SkunkApp.main(null);
-//		
-//		assertEquals( SkunkApp.getiTestValue_ForApp(), CONSTANT_RUN_SUCCESSFUL );
-//	}
+private static final int CONSTANT_RUN_SUCCESSFUL = 100;	
+
+	@Test
+	public void testing_SkunkApp_main()
+	{
+		assertEquals( SkunkApp.getiTestValue_ForApp(), -1 );
+		SkunkApp.main(null);
+		assertEquals( SkunkApp.getiTestValue_ForApp(), CONSTANT_RUN_SUCCESSFUL );
+	}
 }
